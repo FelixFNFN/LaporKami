f82bc4c3d884130f95e9b968c4ef724bfc4c3411
FelixFn <83489247+FelixFNFN@users.noreply.github.com>
2022-12-26 13:31:53 +0700

    Initial commit

---
+++ app/src/main/java/com/example/laporkami/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.laporkami
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+
+class MainActivity : AppCompatActivity() {
+    lateinit var etEmail:EditText
+    lateinit var etPassword:EditText
+    lateinit var btnLogin:Button
+    lateinit var btnRegister:Button
+    lateinit var arrUser:ArrayList<Users>
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        etEmail=findViewById(R.id.etEmail)
+        etPassword=findViewById(R.id.etPassword)
+        btnLogin=findViewById(R.id.btnLogin)
+        btnRegister=findViewById(R.id.btnRegister)
+
+        var intentHome=Intent(this,HomeActivity::class.java)
+        var intentRegister=Intent(this,RegisterActivity::class.java)
+
+        var MainLauncher=registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+                result:ActivityResult ->
+            if(result != null){
+
+            }
+        }
+
+        btnLogin.setOnClickListener {
+            if(etEmail.text.toString()!="" && etPassword.text.toString()!=""){
+                MainLauncher.launch(intentHome)
+            }
+
+        }
+
+        var registerLauncher=registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            result:ActivityResult ->
+            if(result != null){
+
+            }
+        }
+
+        btnRegister.setOnClickListener {
+            registerLauncher.launch(intentRegister)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/laporkami/Users.kt
@@ -0,0 +1,4 @@
+package com.example.laporkami
+
+class Users(var email:String,var password:String) {
+}
