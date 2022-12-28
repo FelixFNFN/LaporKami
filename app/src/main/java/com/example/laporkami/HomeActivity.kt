705a78c5d0241e1900a8478723e36f94afa8c407
FelixFn <83489247+FelixFNFN@users.noreply.github.com>
2022-12-28 22:23:51 +0700

    Dependency install + sambung fragment
    semua dependency sejauh ini sudah di insert + fragment dari page awal sudah tersambung

--- app/src/main/java/com/example/laporkami/HomeActivity.kt
+++ app/src/main/java/com/example/laporkami/HomeActivity.kt
@@ -1,18 +1,56 @@
 package com.example.laporkami
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.example.laporkami.databinding.ActivityHomeBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class HomeActivity : AppCompatActivity() {
     private val coroutine = CoroutineScope(Dispatchers.IO)
+    lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
         homePage()
 
+        binding.navBar.setOnItemSelectedListener {
+
+            if(it.itemId==R.id.home_menu){
+                homePage()
+            }
+            else{
+                laporPage()
+            }
+
+            return@setOnItemSelectedListener true
+        }
+
+
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId==R.id.logout_menu){
+            finish()
+        }
+        else{
+            var profileIntent=Intent()
+            startActivity(profileIntent)
+        }
+        return true
     }
 
     fun homePage() {
@@ -37,4 +75,11 @@ class HomeActivity : AppCompatActivity() {
 //        }
     }
 
+    fun laporPage(){
+        val fragment = LaporFragment()
+        var bundle = Bundle()
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentHome,fragment)
+            .setReorderingAllowed(true).commit()
     }
+}
--- app/src/main/java/com/example/laporkami/HomeFragment.kt
+++ app/src/main/java/com/example/laporkami/HomeFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,6 +23,9 @@ class HomeFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    lateinit var etNIK:EditText
+    lateinit var btnCari:Button
+    lateinit var lvNotif:ListView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,4 +43,11 @@ class HomeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btnCari=view.findViewById(R.id.btnCari)
+        etNIK=view.findViewById(R.id.etNIK)
+        lvNotif=view.findViewById(R.id.listNotif)
+
+    }
 }
