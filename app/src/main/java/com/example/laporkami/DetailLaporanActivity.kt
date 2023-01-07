1083e5535ceb5157661aff587e9229b42bfb866b
erland-20 <79450864+erland-20@users.noreply.github.com>
2023-01-07 13:41:38 +0700

    perubahan tombol like dan dislike serta jumlah comment & Error saat membuat Activity Detail laporan

---
+++ app/src/main/java/com/example/laporkami/DetailLaporanActivity.kt
@@ -0,0 +1,11 @@
+package com.example.laporkami
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailLaporanActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_laporan)
+    }
+}
